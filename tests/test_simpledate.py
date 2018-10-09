54021129437aacb439d1f488ea79e9a3e65c0459
GigiusB <g.bronzini@gmail.com>
2018-10-09 12:36:37 +0100

    Implemented identity

--- tests/test_simpledate.py
+++ tests/test_simpledate.py
@@ -7,3 +7,13 @@ class TestBasicDate:
         d = '{"_type": "BasicDate", "value": "20180117"}'
         assert json.loads(d, cls=BasicDateDecoder) == BasicDate('17/01/2018')
         assert json.dumps(BasicDate('17/01/2018'), cls=BasicDateEncoder) == d
+
+    def test_as_key(self):
+        a, b, c = BasicDate('17/01/2018'), BasicDate('17/01/2018'), BasicDate('18/01/2018')
+        assert a == b
+        assert a != c
+        assert type(a) == type(b) == type(c)
+        assert a is b
+        assert a is not c
+        assert a == c - 1
+
