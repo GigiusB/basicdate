b36a5c8712d157d94d6dc5f19d6f08387fabeb8a
GigiusB <g.bronzini@gmail.com>
2018-10-09 13:23:21 +0100

    Added tests

--- tests/test_simpledate.py
+++ tests/test_simpledate.py
@@ -8,7 +8,7 @@ class TestBasicDate:
         assert json.loads(d, cls=BasicDateDecoder) == BasicDate('17/01/2018')
         assert json.dumps(BasicDate('17/01/2018'), cls=BasicDateEncoder) == d
 
-    def test_as_key(self):
+    def test_identity(self):
         a, b, c = BasicDate('17/01/2018'), BasicDate('17/01/2018'), BasicDate('18/01/2018')
         assert a == b
         assert a != c
@@ -16,4 +16,18 @@ class TestBasicDate:
         assert a is b
         assert a is not c
         assert a == c - 1
+
+    def test_as_key(self):
+        a, b, c = BasicDate('17/01/2018'), BasicDate('17/01/2018'), BasicDate('11/01/2018')
+        assert a is b
+        assert a is not c
+        d = {}
+        a = d.setdefault(a, {'one': 1})
+        b = d.setdefault(b, {'two': 2})
+        c = d.setdefault(c, {'three': 3})
+        assert len(d) == 2
+        assert BasicDate('17/01/2018') in d
+        assert BasicDate('18/01/2018') not in d
+        assert a == b == {'one': 1}
+        assert a != c
 
